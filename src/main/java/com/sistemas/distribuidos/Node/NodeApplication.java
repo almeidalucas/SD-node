08d57ff968d70fba726a65a236c916573852b317
almeida_lucas <almeida.lucas92@gmail.com>
2018-06-08 15:31:51 -0300

    Criada estrutura inicial do Nó

---
+++ src/main/java/com/sistemas/distribuidos/Node/NodeApplication.java
@@ -0,0 +1,40 @@
+package com.sistemas.distribuidos.Node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Lucas Almeida
+ * Realiza configurações necessárias para ouvir serviço WebSocket
+ * Inicia serviço e mantém no ar ouvindo a url determinada*/
+@SpringBootApplication
+public class NodeApplication {
+
+  public static void main(String[] args) {
+
+    List<Transport> transports = new ArrayList<>();
+    transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+
+    SockJsClient sockJsClient = new SockJsClient(transports);
+    WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+    stompClient.setMessageConverter(new StringMessageConverter());
+    stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
+
+    String url = "ws://localhost:8090/sistemas-distribuidos";
+    StompSessionHandler sessionHandler = new NodeHandler();
+    stompClient.connect(url, sessionHandler);
+
+    SpringApplication.run(NodeApplication.class, args);
+  }
+}
---
+++ src/main/java/com/sistemas/distribuidos/Node/NodeHandler.java
@@ -0,0 +1,42 @@
+package com.sistemas.distribuidos.Node;
+
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+
+import java.lang.reflect.Type;
+
+/**
+ * @author Lucas Almeida
+ * Classe criada para tratar eventos
+ * */
+public class NodeHandler extends StompSessionHandlerAdapter {
+
+  /*Assim que subir o projeto o método é executado*/
+  @Override
+  public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+    session.subscribe("/ws-client/hello", this);
+    session.send("/app/hello", "Nó conectado!");
+
+    System.out.println("Conectado!");
+  }
+
+  /*Executa caso aconteça algum problema*/
+  @Override
+  public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+    exception.printStackTrace();
+  }
+
+  /*Define o tipo do retorno*/
+  @Override
+  public Type getPayloadType(StompHeaders headers) {
+    return String.class;
+  }
+
+  /*Assim que uma chamada for feita para url inscrita esse método é executado*/
+  @Override
+  public void handleFrame(StompHeaders headers, Object payload) {
+    System.out.println("Recebido com sucesso: " + payload.toString());
+  }
+}
